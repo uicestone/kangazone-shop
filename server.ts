2aa17f72e0b43bd5e4918974a4635be80d999231
koishi <ncysatnaf@gmail.com>
2019-08-30 18:12:48 +0800

    update server

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ const { PORT = 5000 } = process.env;
 const app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
