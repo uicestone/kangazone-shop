d14544172b79bf868c87e767e2c0ca6a26d9d55e
koishi <ncysatnaf@gmail.com>
2019-08-30 18:20:00 +0800

    add gizp

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import serveStatic from "serve-static";
+import compression from "compression";
 
 const { PORT = 5000 } = process.env;
 
 const app = express();
+app.use(compression());
 app.use(serveStatic(__dirname + "/dist"));
 
 app.get("*", (req, res) => {
