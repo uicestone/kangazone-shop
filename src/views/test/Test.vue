a3d4f61e959402f64fb6ef948a4358035f704435
ymc <ymc.try@realyoung.net>
2019-09-02 17:26:00 +0800

    js call android example

---
+++ src/views/test/Test.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+
+    div
+        p {{msg}}
+
+
+</template>
+
+<script>
+  export default {
+    name: "Test",
+    data() {
+      return {
+        msg: "哈哈"
+      }
+    },
+    created(){
+      this.showAndroidToast()
+    },
+    mounted() {
+      //将要给原生调用的方法挂载到 window 上面
+      window.callJsFunction = this.callJsFunction
+    },
+    methods: {
+      callJsFunction(str) {
+        this.msg = str;
+        return "js调用成功"
+      },
+      showAndroidToast() {
+        $App.jsCallAndroid("")
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
