14723b30d77725f7314548a068fce02a279b2e94
Uice Lu <uicestone@gmail.com>
2019-09-08 20:20:30 +0800

    Revert "utf-8 to gbk"
    This reverts commit d74bffd36f2ce82b9f7cd91a167a1a5bebefa39e.

--- src/views/test/Test.vue
+++ src/views/test/Test.vue
@@ -42,7 +42,6 @@ import { _ } from "../../utils/lodash";
 import { sendPaymentToSunmi } from "../../services/payment";
 import EscPosEncoder from "@xinghe/esc-pos-encoder";
 import { Buffer } from "buffer/";
-import { encodeGBK, decodeGBK } from "gbk-string";
 
 let encoder = new EscPosEncoder();
 
@@ -125,14 +124,9 @@ export default {
       let result = encoder
         .initialize()
         .codepage(this.printForm.encodingType)
-        .line(encodeGBK(this.printForm.content))
+        .line(this.printForm.content)
         .encode();
-      console.log(encodeGBK(this.printForm.content));
-      console.log(result);
-
       result = Buffer.from(result).toString("hex"); //?
-      console.log(result);
-
       $App.jsPrint(this.printForm.venderId, result);
     },
     selectType(val) {
