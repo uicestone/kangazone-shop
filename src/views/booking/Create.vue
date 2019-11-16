6224b3d16aae65b6adc5948c0284dfbc8dd23ac4
koishi <ncysatnaf@gmail.com>
2019-11-16 19:02:18 +0800

    load user from query string

--- src/views/booking/Create.vue
+++ src/views/booking/Create.vue
@@ -42,7 +42,7 @@
                   @change="goCheckIn"
                 ) 
                 v-btn(color="primary" :block="!searchUserForm.bookings.length" v-if="userValid" @click="createBookingFromSearchUser") 创建新预约
-          v-card.py-4.px-7(v-if="$_.get(searchUserForm, 'user.id')" style="flex:2")
+          v-card.py-4.px-7(v-if="userValid" style="flex:2")
             div(v-if="!searchUserForm.user.cardNo && (searchUserForm.user.codes.length || searchUserForm.user.credit)")
               v-text-field(label="绑定会员卡" v-model="searchUserForm.cardNo" required :rules="[v => !!v || '请输入卡号']" clearable type="number" autocomplete="off")
               v-btn(color="primary" :disabled="!searchUserForm.cardNo" :loading="searchUserForm.bindCard_loading" @click="handleBindCardNo") 绑定卡号
@@ -325,7 +325,17 @@ export default {
       this.searchUserForm.bookings_loading = false;
     }
   },
+  beforeMount() {
+    this.parseQuery();
+  },
   methods: {
+    async parseQuery() {
+      const { userId } = this.$route.query;
+      if (userId) {
+        const { data: user } = await getUser({ id: userId });
+        this.searchUserForm.user = user;
+      }
+    },
     async updatePrice() {
       if (this.createBookingForm.loading_price) return;
       this.createBookingForm.loading_price = true;
